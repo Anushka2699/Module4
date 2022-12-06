0809e287f8ae0dd24daa252a825036f511294284
Anushka2699 <108417245+Anushka2699@users.noreply.github.com>
2022-12-06 13:47:44 +0530

    updated

--- Module.js
+++ Module.js
@@ -11,15 +11,6 @@ names[8]="laura";
 names[9]="jim";
 
 
-// for (var i = 0; i < names.length; i++) {
-// 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-//         console.log("Goodbye "+ names[i])
-// 	}
-// 	else{
-// 		console.log("Hello "+ names[i])
-// 	}
-// }
-
 for (var i=0; i<names.length; i++){
     if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("goodbye "+names[i])
