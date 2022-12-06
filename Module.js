a76da87add2acf891be0657bce738b31cdc61012
Anushka2699 <108417245+Anushka2699@users.noreply.github.com>
2022-12-06 13:38:29 +0530

    update

--- Module.js
+++ Module.js
@@ -1,21 +1,30 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
+names[0]="laya";
+names[1]="Jeevan";
+names[2]="preetham";
+names[3]="jaga";
+names[4]="ram";
+names[5]="charan";
+names[6]="yaksh";
+names[7]="laksh";
 names[8]="laura";
 names[9]="jim";
 
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
+// for (var i = 0; i < names.length; i++) {
+// 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+//         console.log("Goodbye "+ names[i])
+// 	}
+// 	else{
+// 		console.log("Hello "+ names[i])
+// 	}
+// }
+
+for (var i=0; i<names.length; i++){
+    if(names[i].charAt(0)==='J'|| names[i].chartAt(0)==='j'){
+        console.log("goodbye"+names[i])
+    }
+    else{
+        console.log("hello"+names[i])
+    }
 }
